8d7a4e90904b118b0706d71e009de3b79c2014cc
Franco Sarachu <fsarachu@gmail.com>
2018-05-21 17:49:40 -0300

    Add basic auth routing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,74 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {connect} from "react-redux";
+import {BrowserRouter, Switch, Route, Redirect} from "react-router-dom";
+
+import {loadApp} from "../../redux/modules/app/actions";
+import {errorLoading, isLoading} from "../../redux/modules/app/selectors";
 
 import './App.css';
-import FbSdkProvider from "../FbSdkProvider/FbSdkProvider";
+import Login from "../../views/Login/Login";
+import Me from "../../views/Me/Me";
+import ProtectedRoute from "../ProtectedRoute/ProtectedRoute";
 
 class App extends Component {
+
+    componentDidMount() {
+        this.props.loadApp();
+    }
+
     render() {
+
+        const {isLoading, errorLoading} = this.props;
+
+        if(isLoading) {
+            return <h1>Loading</h1>;
+        }
+
+        if(errorLoading) {
+            return <h1>ERROR: {errorLoading}</h1>;
+        }
+
         return (
-            <FbSdkProvider>
-                <div className="App">
-                    <h1>Hello World</h1>
-                </div>
-            </FbSdkProvider>
+            <BrowserRouter>
+                <Switch>
+                    <Route
+                        path='/login'
+                        component={Login}
+                    />
+
+                    <ProtectedRoute
+                        path='/'
+                        component={Me}
+                    />
+
+
+                    <Route render={() => <Redirect to='/'/>}/>
+
+                </Switch>
+            </BrowserRouter>
         );
     }
 }
 
-export default App;
+App.propTypes = {
+    isLoading: PropTypes.bool.isRequired,
+    errorLoading: PropTypes.string,
+    loadApp: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => {
+    return {
+        isLoading: isLoading(state),
+        errorLoading: errorLoading(state),
+    };
+};
+
+const mapDispatchToProps = (dispatch) => ({
+    loadApp() {
+        dispatch(loadApp());
+    }
+});
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
