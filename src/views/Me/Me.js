387170754abc102722783fed1b7e5738235c9f74
Franco Sarachu <fsarachu@gmail.com>
2018-05-21 17:44:03 -0300

    Add dummy "Me" view

---
+++ src/views/Me/Me.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Me extends Component {
+    render() {
+        return (
+            <div className='Me'>
+                <h1>Me</h1>
+            </div>
+        );
+    }
+}
+
+export default Me;
