d93f851cd8a74d4dfdb20e6760193abcc2e2f48b
Franco Sarachu <fsarachu@gmail.com>
2018-05-29 19:52:36 -0300

    Add getPages saga

--- src/redux/modules/facebook/sagas.js
+++ src/redux/modules/facebook/sagas.js
@@ -1,6 +1,6 @@
 import {all, call, put, takeEvery} from "redux-saga/effects";
 
-import {FB_LOGIN, fbLoginFailure, fbLoginSuccess} from "./actions";
+import {FB_LOGIN, fbLoginFailure, fbLoginSuccess, GET_PAGES, getPagesSuccess, getPagesFailure} from "./actions";
 import * as service from './service';
 
 export function* fbLogin() {
@@ -20,8 +20,25 @@ export function* watchFbLogin() {
 }
 
 
+export function* getPages() {
+    try {
+        const pages = yield call(service.getPages);
+        yield put(getPagesSuccess(pages));
+    } catch (error) {
+        console.error(error);
+        yield put(getPagesFailure("Couldn't get user pages"));
+
+    }
+}
+
+export function* watchGetPages() {
+    yield takeEvery(GET_PAGES, getPages);
+}
+
+
 export default function* rootSaga() {
     yield all([
         watchFbLogin(),
+        watchGetPages(),
     ]);
 };
