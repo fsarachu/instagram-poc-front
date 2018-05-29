7231cc01f2c6d175d765b03d6b46cf0b132f25c3
Franco Sarachu <fsarachu@gmail.com>
2018-05-29 19:49:12 -0300

    Add selectors for new stuff

--- src/redux/modules/facebook/selectors.js
+++ src/redux/modules/facebook/selectors.js
@@ -6,8 +6,36 @@ export function accessToken(state) {
     return getState(state).accessToken;
 }
 
+export function userId(state) {
+    return getState(state).userId;
+}
+
+export function userName(state) {
+    return getState(state).userName;
+}
+
+export function userPicture(state) {
+    return getState(state).userPicture;
+}
+
 export function hasAccessToken(state) {
     return !!accessToken(state);
+}
+
+export function isFetchingPages(state) {
+    return getState(state).isFetchingPages;
+}
+
+export function errorFetchingPages(state) {
+    return getState(state).errorFetchingPages;
+}
+
+export function pages(state) {
+    return getState(state).pages;
+}
+
+export function selectedPageId(state) {
+    return getState(state).selectedPageId;
 }
 
 export function isLoggingIn(state) {
