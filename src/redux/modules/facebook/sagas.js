01615a390fd688d7fce435c0ba79c898287248d7
Franco Sarachu <fsarachu@gmail.com>
2018-05-29 19:52:11 -0300

    fbLogin saga now fetches user info too

--- src/redux/modules/facebook/sagas.js
+++ src/redux/modules/facebook/sagas.js
@@ -1,12 +1,13 @@
 import {all, call, put, takeEvery} from "redux-saga/effects";
 
 import {FB_LOGIN, fbLoginFailure, fbLoginSuccess} from "./actions";
-import {login} from './service';
+import * as service from './service';
 
-export function* fbLogin(action) {
+export function* fbLogin() {
     try {
-        const accessToken = yield call(login);
-        yield put(fbLoginSuccess(accessToken));
+        const accessToken = yield call(service.login);
+        const profile = yield call(service.getUser);
+        yield put(fbLoginSuccess(accessToken, profile.id, profile.name, profile.picture.data.url));
     } catch (error) {
         console.error(error);
         yield put(fbLoginFailure("Couldn't login to Facebook"));
