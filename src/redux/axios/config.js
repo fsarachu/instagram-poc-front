ab49e76f0a81dc7a8e6fd90742c521e6aba9dbfe
Franco Sarachu <fsarachu@gmail.com>
2018-06-01 15:31:14 -0300

    Add href to post card

--- src/redux/axios/config.js
+++ src/redux/axios/config.js
@@ -1,5 +1,6 @@
 export default {
-    baseURL: process.env.NODE_ENV === 'production' ? 'https://ig-poc.herokuapp.com' : 'http://localhost:5050',
+    // baseURL: process.env.NODE_ENV === 'production' ? 'https://ig-poc.herokuapp.com' : 'http://localhost:5050',
+    baseURL: 'https://ig-poc.herokuapp.com',
     headers: {
         'Accept': 'application/json, text/plain, */*',
         'Content-Type': 'application/json'
