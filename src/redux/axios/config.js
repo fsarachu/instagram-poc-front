fc9b2063f9252f921b1261cb60a5f2eccb7c9a70
Franco Sarachu <fsarachu@gmail.com>
2018-06-01 12:32:25 -0300

    Api baseURL environment dynamic

--- src/redux/axios/config.js
+++ src/redux/axios/config.js
@@ -1,5 +1,5 @@
 export default {
-    baseURL: 'http://localhost:5050',
+    baseURL: process.env.NODE_ENV === 'production' ? 'https://ig-poc.herokuapp.com' : 'http://localhost:5050',
     headers: {
         'Accept': 'application/json, text/plain, */*',
         'Content-Type': 'application/json'
